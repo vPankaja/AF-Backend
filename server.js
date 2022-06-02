368ed80074f9b30c71124c316015e045cef19455
Shenal99 <shenal3332018@gmail.com>
2022-06-02 22:13:12 +0530

    supe1

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ import subtypeRoutes from "./routes/subtypeRoutes.js";
 
 import fileUploadController from "./controllers/fileuploadController.js";
 
+import SupEvaluation from "./routes/SupEvaluation.js"
 
 dotenv.config();
 
@@ -37,6 +38,7 @@ app.use("/subtype", subtypeRoutes);
 
 app.use('/api/files/', fileUploadController)
 
+app.use("/supEve",SupEvaluation);
 
 const __dirname = path.resolve()
 //set upload folder
